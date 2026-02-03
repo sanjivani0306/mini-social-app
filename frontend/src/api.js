bdaedd6ee6febfb6c628944ab46a0534a62018a9
Sanjivani Tulugu <tulugusanjivani2gmail.com>
2026-02-03 15:39:21 +0530

    Add production API URL

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,15 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "https://mini-social-app-ypol.onrender.com"
-});
-
-API.interceptors.request.use((req) => {
-  const token = localStorage.getItem("token");
-  if (token) {
-    req.headers.authorization = token;
-  }
-  return req;
+  baseURL: process.env.REACT_APP_API_URL,
 });
 
 export default API;
